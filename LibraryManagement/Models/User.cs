aca1e9d3f7e57badfcb8147ecfc6b93ea3919e51
mirunadurac <48506996+mirunadurac@users.noreply.github.com>
2020-04-13 15:13:31 +0300

    Create User.cs
    Add User

---
+++ LibraryManagement/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagement.Models
+{
+    class User
+    {
+        protected static int Id;
+        int IdUser;
+        public string FirstName;
+        public string LastName;
+        public List<Book> BorrowedBooks { get; set; }
+
+        public User(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id++;
+            IdUser = Id;
+            BorrowedBooks = new List<Book>();
+        }
+    }
+}
