70f34148977d17be8a71aa01fea765f0fe0d2a92
heroiuflorina <48507109+heroiuflorina@users.noreply.github.com>
2020-04-23 15:31:00 +0300

    Adding functions

--- LibraryManagement/Flyweight/BookRegister.cs
+++ LibraryManagement/Flyweight/BookRegister.cs
@@ -8,11 +8,27 @@ namespace LibraryManagement.Flyweight
     {
         public Dictionary<EBookMaterialType, int> borrowedBook { get; set; }
 
-        public void CountBorrowedType(EBookMaterialType type)
-        {
+        //public void CountBorrowedType(EBookMaterialType type)
+        //{
 
-          
+
+        //}
+
+        public void AddABook(EBookMaterialType type)
+        {
+            if (borrowedBook.ContainsKey(type))
+                borrowedBook[type]++;
+            else borrowedBook.Add(type, 1);
         }
+
+        public void SeeReport()
+        {
+            foreach (var it in borrowedBook)
+            {
+                Console.WriteLine(it.Key + " " + it.Value.ToString());
+            }
+        }
+
 
         public abstract BookFormat CreateNewBook();
         public abstract bool IsAvailable(double value);
--- LibraryManagement/Flyweight/Report.cs
+++ LibraryManagement/Flyweight/Report.cs
@@ -9,5 +9,35 @@ namespace LibraryManagement.Flyweight
         RegisterMagazine RegisterMagazine { get; set; } = new RegisterMagazine();
         RegisterNewspaper RegisterNewspaper { get; set; } = new RegisterNewspaper();
         RegisterPaperBook RegisterPaperBook { get; set; } = new RegisterPaperBook();
+
+
+        private BookRegister GetBookRegister(EBookMaterialType type)
+        {
+            BookRegister register = null;
+
+            switch (type)
+            {
+                case EBookMaterialType.Magazine:
+                    register = RegisterMagazine;
+                    break;
+                case EBookMaterialType.Newspaper:
+                    register = RegisterNewspaper;
+                    break;
+                case EBookMaterialType.PaperBook:
+                    register = RegisterPaperBook;
+                    break;
+            }
+
+            return register;
+        }
+        public void AddNewBook(EBookMaterialType type)
+        {
+            GetBookRegister(type).AddABook(type);
+        }
+
+        public void SeeReport()
+        {
+            RegisterNewspaper.SeeReport();
+        }
     }
 }
