b6a182fbcb98b191bca41ab14f00b785f36b2b07
heroiuflorina <48507109+heroiuflorina@users.noreply.github.com>
2020-04-23 20:57:35 +0300

    Create Library

---
+++ LibraryManagement/Singleton/Library.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagement.Singleton
+{
+    public class Library
+    {
+        private static object padlock = new object();
+        private static Library instance;
+
+        public static Library Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Library();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        private Library()
+        {
+
+        }
+
+    }
+}
