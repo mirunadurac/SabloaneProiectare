6b90d288616840f80f4c18a8ddcca200ea3958ab
Bogdan <bogdybv25@gmail.com>
2020-04-23 17:45:17 +0300

    Added server and client projects.

---
+++ LibraryServer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace LibraryServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartServer();
+        }
+
+        public static void StartServer()
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            try
+            {
+                Socket handler = null;
+                // Create a Socket that will use Tcp protocol      
+                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                // A Socket must be associated with an endpoint using the Bind method  
+                listener.Bind(localEndPoint);
+
+                try
+                {
+                   
+                    // Specify how many requests a Socket can listen before it gives Server busy response.  
+                    // We will listen 10 requests at a time  
+                    listener.Listen(10);
+
+                    Console.WriteLine("Waiting for a connection...");
+                    while (true)
+                    {
+                        handler = listener.Accept();
+                        ServerThread server = new ServerThread();
+                        server.StartServerThread(handler);
+                    }
+
+                    
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                finally 
+                {
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch(Exception e)
+                {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+    }
+}
---
+++ LibraryServer/ServerThread.cs
@@ -0,0 +1,60 @@
+﻿using LibraryManagement.FactoryMethod;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace LibraryServer
+{
+    public class ServerThread
+    {
+        private Socket clientSocket;
+        public void StartServerThread(Socket inClientSocket)
+        {
+            clientSocket = inClientSocket;
+            Thread ctThread = new Thread(Execute);
+            ctThread.Start();
+        }
+        private void Execute()
+        {
+            // Incoming data from the client. 
+            try
+            {
+                string data = null;
+                byte[] bytes = null;
+
+                while (true)
+                {
+                    bytes = new byte[1024];
+                    int bytesRec = clientSocket.Receive(bytes);
+                    data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                    var fooReceived = JToken.Parse(data).ToObject<Fiction>();
+                    Console.WriteLine("Text received : {0}", data);
+
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+                    clientSocket.Send(msg);
+
+                    if (data.IndexOf("Stop") > -1)
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+        }
+    }
+}
